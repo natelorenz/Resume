103e62746f18489ed07f946c938689c18bf26b6a
natelorenz <natelorenz@knights.ucf.edu>
2017-02-19 19:48:14 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an online resume for web design workshop.
---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('#textarea1').trigger('autoresize');
+
+    $('.collapse').collapsible('setup1');
+  });
+})(jQuery);
